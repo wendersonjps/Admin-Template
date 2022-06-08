10d4136359b381fa20b8d4bcffd624dd5600a467
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-08 18:59:40 -0300

    :hammer: Integrating Layout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import Layout from '../components/template/Layout'
+
 export default function Home() {
   return (
-    <div className={`flex justify-center h-screen bg-gradient-to-r from-green-500 to-blue-500`}>
-      <h1>Admin</h1>
-    </div>
+    <Layout title="Página Inicial" subtitle="Template em construção!">
+      <h3>Conteúdo!</h3>
+    </Layout>
   )
 }
