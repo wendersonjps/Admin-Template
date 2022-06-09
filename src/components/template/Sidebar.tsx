33c114385042f870ec314e5759732c82bcf14119
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-09 19:01:21 -0300

    :hammer: Integrating a new menu item

--- src/components/template/Sidebar.tsx
+++ src/components/template/Sidebar.tsx
@@ -1,17 +1,20 @@
 import MenuItem from './MenuItem'
-import { IconHome, IconNotification, IconSettings } from '../icons/index'
+import { IconHome, IconLogout, IconNotification, IconSettings } from '../icons/index'
 import Logo from './Logo'
 
 export default function Sidebar() {
     return (
-        <aside>
-            <div className={` flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-800 h-20 w-20`}>
+        <aside className={`flex flex-col`}>
+            <div className={`flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-800 h-20 w-20`}>
                 <Logo />
             </div>
-            <ul>
+            <ul className={`flex-grow`}>
                 <MenuItem url='/' text='Início' icon={IconHome} />
                 <MenuItem url='/settings' text='Ajustes' icon={IconSettings} />
                 <MenuItem url='/notifications' text='News!' icon={IconNotification} />
+            </ul>
+            <ul>
+                <MenuItem text='Sair' icon={IconLogout} onClick={() => console.log('logout')} className={`text-red-500 hover:bg-red-400 hover:text-white`} />
             </ul>
         </aside>
     )
