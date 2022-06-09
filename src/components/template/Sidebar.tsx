233535d937bb5ff362d327ea2fdb580143f4aaa5
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-09 18:11:45 -0300

    :hammer: Integrating Logo component

--- src/components/template/Sidebar.tsx
+++ src/components/template/Sidebar.tsx
@@ -1,9 +1,13 @@
 import MenuItem from './MenuItem'
 import { IconHome, IconNotification, IconSettings } from '../icons/index'
+import Logo from './Logo'
 
 export default function Sidebar() {
     return (
         <aside>
+            <div className={` flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-800 h-20 w-20`}>
+                <Logo />
+            </div>
             <ul>
                 <MenuItem url='/' text='Início' icon={IconHome} />
                 <MenuItem url='/settings' text='Ajustes' icon={IconSettings} />
