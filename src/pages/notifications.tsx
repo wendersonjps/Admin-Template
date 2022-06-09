935f34974c41e20a5637d4c5cc8e02456777fe95
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-08 21:51:25 -0300

    :sparkles: Added new pages

---
+++ src/pages/notifications.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/template/Layout'
+
+export default function Notifications() {
+  return (
+    <Layout title="Notificações" subtitle="Novidades aqui!">
+      <h3>Notificações!</h3>
+    </Layout>
+  )
+}
---
+++ src/pages/settings.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/template/Layout'
+
+export default function Settings() {
+  return (
+    <Layout title="Ajustes e configurações" subtitle="Ajustes do sistema!">
+      <h3>Ajustes!</h3>
+    </Layout>
+  )
+}
