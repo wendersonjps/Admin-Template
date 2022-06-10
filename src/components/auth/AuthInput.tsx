6d8d596042570016088a101220cf5936b98621ef
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-10 18:58:26 -0300

    :lipstick: Added css inline

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -9,9 +9,9 @@ interface AuthInputProps {
 
 export default function AuthInput(props: AuthInputProps) {
     return props.dontRender ? null : (
-        <div className={`flex flex-col`}>
+        <div className={`flex flex-col mt-4`}>
             <label>{props.label}</label>
-            <input type={props.type ?? 'text'} value={props.value} onChange={e => props.valueChanged?.(e.target.value)} required={props.required} />
+            <input type={props.type ?? 'text'} value={props.value} onChange={e => props.valueChanged?.(e.target.value)} required={props.required} className={`px-4 py-3 mt-2 rounded-lg border bg-gray-100 focus:bg-white`} />
         </div>
     )
 }
