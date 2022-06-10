117d20d4ed4199bd1a2c0f7d8d0bfaad5e643ca2
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-10 18:27:39 -0300

    :fire: Removed useless function

--- src/pages/notifications.tsx
+++ src/pages/notifications.tsx
@@ -1,12 +1,9 @@
 import Layout from '../components/template/Layout'
-import useAppData from '../data/hook/useAppData'
 
 export default function Notifications() {
-  const { themeToggle } = useAppData()
-
   return (
     <Layout title="Notificações" subtitle="Novidades aqui!">
-      <button onClick={themeToggle}>Alternar Tema!</button>
+      <h1>Notificações!</h1>
     </Layout>
   )
 }
