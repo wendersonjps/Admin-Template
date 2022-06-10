d4b574b26c11fb17055ec45da96e1848b1f9ff0b
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-10 13:52:08 -0300

    :hammer: Integrating ThemeButton component

--- src/components/template/Header.tsx
+++ src/components/template/Header.tsx
@@ -1,4 +1,6 @@
 import Title from './Title'
+import { ThemeButton } from './ThemeButton'
+import useAppData from '../../data/hook/useAppData'
 
 interface HeaderProps {
     title: string
@@ -6,9 +8,14 @@ interface HeaderProps {
 }
 
 export default function Header(props: HeaderProps) {
+    const { theme, themeToggle } = useAppData()
+
     return (
-        <div>
+        <div className={`flex`}>
             <Title title={props.title} subtitle={props.subtitle} />
+            <div className={`flex flex-grow justify-end`}>
+                <ThemeButton theme={theme} themeToggle={themeToggle} />
+            </div>
         </div>
     )
 }
