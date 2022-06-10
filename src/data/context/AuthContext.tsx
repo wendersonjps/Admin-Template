dedd6800516c1a142c1d6a6c35908e3c4c7954d3
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-10 20:32:51 -0300

    :sparkles: Added AuthContext component

---
+++ src/data/context/AuthContext.tsx
@@ -0,0 +1,13 @@
+import firebase from '../../firebase/config'
+
+interface AuthContextProps {
+
+}
+
+export default function AuthContext(props: AuthContextProps) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
