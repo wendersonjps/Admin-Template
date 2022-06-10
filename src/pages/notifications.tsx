d0cdfe179db6c2d502f63f8efe6da8278a630ba3
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-09 21:23:12 -0300

    :hammer: Integrating Context API

--- src/pages/notifications.tsx
+++ src/pages/notifications.tsx
@@ -1,9 +1,12 @@
 import Layout from '../components/template/Layout'
+import useAppData from '../data/hook/useAppData'
 
 export default function Notifications() {
+  const data = useAppData()
+
   return (
     <Layout title="Notificações" subtitle="Novidades aqui!">
-      <h3>Notificações!</h3>
+      <h3>{data.name}</h3>
     </Layout>
   )
 }
