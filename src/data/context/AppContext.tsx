e24137e35a2c2a96cf8f3b31dc57f02a7fbd14f3
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-09 21:45:05 -0300

    :hammer: Creating a function to switch theme

--- src/data/context/AppContext.tsx
+++ src/data/context/AppContext.tsx
@@ -1,13 +1,25 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 
-const AppContext = createContext({
-    name: null
-})
+type Theme = 'dark' | ''
+
+interface AppContextProps {
+    theme?: Theme
+    themeToggle?: () => void
+}
+
+const AppContext = createContext<AppContextProps>({})
 
 export function AppProvider(props) {
+    const [theme, setTheme] = useState<Theme>('')
+
+    function themeToggle() {
+        setTheme(theme === '' ? 'dark' : '')
+    }
+
     return (
         <AppContext.Provider value={{
-            name: 'Teste Context API'
+            theme,
+            themeToggle
         }}>
             {props.children}
         </AppContext.Provider>
