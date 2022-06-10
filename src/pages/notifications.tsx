221ca4f00198b7132d106c6dbbeeddf5e5e11ea3
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-09 21:44:19 -0300

    :hammer: Implementing a example for switch dark mode

--- src/pages/notifications.tsx
+++ src/pages/notifications.tsx
@@ -2,11 +2,11 @@ import Layout from '../components/template/Layout'
 import useAppData from '../data/hook/useAppData'
 
 export default function Notifications() {
-  const data = useAppData()
+  const { themeToggle } = useAppData()
 
   return (
     <Layout title="Notificações" subtitle="Novidades aqui!">
-      <h3>{data.name}</h3>
+      <button onClick={themeToggle}>Alternar Tema!</button>
     </Layout>
   )
 }
