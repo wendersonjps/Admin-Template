21f0ec70a4b3189e55f534dcbc3416d388740460
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-10 21:16:57 -0300

    :hammer: Creating two functions about login and Google login

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -1,13 +1,41 @@
+import { createContext, useState } from 'react'
+import route from 'next/router'
+
 import firebase from '../../firebase/config'
+import User from '../../model/User'
 
 interface AuthContextProps {
-
+    user?: User
+    loginGoogle?: () => Promise<void>
 }
 
-export default function AuthContext(props: AuthContextProps) {
+const AuthContext = createContext<AuthContextProps>({})
+
+async function standardUser(userFirebase: firebase.User): Promise<User> {
+    const token = await userFirebase.getIdToken()
+    return {
+        uid: userFirebase.uid,
+        name: userFirebase.displayName,
+        email: userFirebase.email,
+        token,
+        provider: userFirebase.providerData[0].providerId,
+        imageUrl: userFirebase.photoUrl
+    }
+}
+
+export function AuthProvider(props) {
+    const [user, setUser] = useState<User>(null)
+
+    async function loginGoogle() {
+        console.log('Login Google')
+        route.push('/')
+    }
+
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={{ user, loginGoogle }}>
+            {props.children}
+        </AuthContext.Provider>
     )
 }
+
+export default AuthContext
