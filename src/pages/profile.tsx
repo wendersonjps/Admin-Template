acee53ba32c866ef6ae22d4aadae137044c6e06b
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-13 20:02:23 -0300

    :sparkles: Added a new page for user settings

---
+++ src/pages/profile.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/template/Layout'
+
+export default function Profile() {
+  return (
+    <Layout title="Perfil do Usuário" subtitle="Configurações do usuário!">
+      <h1>Informações do Usuário</h1>
+    </Layout>
+  )
+}
