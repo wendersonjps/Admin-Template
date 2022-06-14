eb8b1b2051eb0ae390fa3af77f65dfe7ca2dc6bd
Wenderson Júnio <wendersonjps@gmail.com>
2022-06-13 22:09:21 -0300

    :hammer: Integrating AuthForce

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -1,6 +1,7 @@
 import Sidebar from './Sidebar'
 import Header from './Header'
 import Content from './Content'
+import ForceAuth from '../auth/ForceAuth'
 import useAppData from '../../data/hook/useAppData'
 
 interface LayoutProps {
@@ -11,16 +12,18 @@ interface LayoutProps {
 
 export default function Layout(props: LayoutProps) {
     const { theme } = useAppData()
-    
+
     return (
-        <div className={`${theme} flex h-screen w-screen`}>
-            <Sidebar />
-            <div className={`flex flex-col w-full p-7 bg-gray-300 dark:bg-gray-800`}>
-                <Header title={props.title} subtitle={props.subtitle} />
-                <Content>
-                    {props.children}
-                </Content>
+        <ForceAuth>
+            <div className={`${theme} flex h-screen w-screen`}>
+                <Sidebar />
+                <div className={`flex flex-col w-full p-7 bg-gray-300 dark:bg-gray-800`}>
+                    <Header title={props.title} subtitle={props.subtitle} />
+                    <Content>
+                        {props.children}
+                    </Content>
+                </div>
             </div>
-        </div>
+        </ForceAuth>
     )
 }
